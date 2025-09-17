532b47db6c8e247dbfd3dbbb928be8937f8b878f
Munib Schule <MunibSchule@Munibs-MacBook-Pro.local>
2025-09-17 10:46:33 +0200

    Initial commit of Maui project

---
+++ Fuhrpark/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Fuhrpark;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Fuhrpark/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Fuhrpark")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Fuhrpark.Pages")]
